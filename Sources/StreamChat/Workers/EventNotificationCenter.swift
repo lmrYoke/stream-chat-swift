c18a5621ad793d009f14b2d12b0de17d9e30076d
Vojta Stavik <stavik@outlook.com>
2021-04-01 18:21:29 +0200

    Implement batching for event processing

--- Sources/StreamChat/Workers/EventNotificationCenter.swift
+++ Sources/StreamChat/Workers/EventNotificationCenter.swift
@@ -11,6 +11,15 @@ class EventNotificationCenter: NotificationCenter {
     /// The database used when evaluating middlewares.
     let database: DatabaseContainer
     
+    /// Events are processed in batches to reduce database writes. This interval is the max number of seconds
+    /// an event can wait before its processing started.
+    ///
+    /// Mutating this value doesn't affect the existing batch and the new value is applied for the following batch.
+    ///
+    var eventBatchPeriod: TimeInterval = 0.5
+    
+    @Atomic var pendingEvents: [Event] = []
+    
     init(middlewares: [EventMiddleware] = [], database: DatabaseContainer) {
         self.database = database
         super.init()
@@ -22,9 +31,34 @@ class EventNotificationCenter: NotificationCenter {
     }
     
     func process(_ event: Event) {
-        middlewares.process(event: event) { [weak self] in
-            guard let self = self, let eventToPublish = $0 else { return }
-            self.post(Notification(newEventReceived: eventToPublish, sender: self))
+        var shouldScheduleProcessing = false
+        _pendingEvents {
+            shouldScheduleProcessing = $0.isEmpty
+            $0.append(event)
+        }
+        
+        if shouldScheduleProcessing {
+            scheduleProcessing()
+        }
+    }
+    
+    /// Starts the timer and schedules the processing of events
+    private func scheduleProcessing() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + eventBatchPeriod) { [weak self] in
+            self?.database.write { session in
+                var eventsToProcess: [Event] = []
+                self?._pendingEvents {
+                    eventsToProcess = $0
+                    $0.removeAll()
+                }
+                
+                eventsToProcess.forEach { event in
+                    self?.middlewares.process(event: event, session: session) {
+                        guard let self = self, let eventToPublish = $0 else { return }
+                        self.post(Notification(newEventReceived: eventToPublish, sender: self))
+                    }
+                }
+            }
         }
     }
 }
